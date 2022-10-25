c1df53d9ab07e0c8bd81e2f9c7e60119790480b3
aidanabaltabekova <aidana.baltabekova@nu.edu.kz>
2022-10-25 12:07:48 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Problem #3
+function AreaOfCircle(x) {
+    let area = PI * x * x;
+    console.log(area);
+}
+ 
+PI=3.14 
+AreaOfCircle(6)
+
+// Problem #9
+const n = -9;
+
+    if (n < 0) 
+        console.log (n + ' is negative');
+    else
+        console.log (n + ' is positive')
+
+// Problem #8
+function UsdToKzt(usd) {
+    let value = rate * usd;
+    console.log(value);
+}
+
+rate=469.86
+UsdToKzt(4)
